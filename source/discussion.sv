ba34eb9426980fc74663c8a80f5439deb1ca3ba9
coin50 <chu285@purdue.edu>
2025-11-17 15:11:32 -0500

    Update discussion.sv

--- source/discussion.sv
+++ source/discussion.sv
@@ -1,4 +1,5 @@
 `default_nettype none
+//
 module shift_pipe_seq(
 	input logic clk, en, rst,
 	input logic [15:0] nxt_pipe,
@@ -12,15 +13,46 @@ module shift_pipe_seq(
 	assign out = Q;
 endmodule
 
-//on clock edge, out = 0 iff low <= bird_y < high
+//collision detection, out = 1 iff collision
+
+`define BIRD_SZ 15
 module collision(
-	input logic clk,
 	input logic [15:0] bird_y, low, high,
 	output logic out
 );
-	always_ff @(posedge clk) begin
-		
+	assign out = (bird_y < low + BIRD_SZ) || (bird_y + BIRD_SZ >= high);
+endmodule
+
+
+//TODO FIGURE OUT NEGATIVE NUMBERS
+`define BIRD_ACC = 1;
+`define JUMP_VEL = 7;
+module update_position(
+	input logic clk,en,rst,
+	input logic jmp,
+	output logic pos
+);
+	logic [15:0] bird_y;
+	logic [15:0] bird_v;
+	logic [15:0] nxt_bird_y;
+	logic [15:0] nxt_bird_v;
+	
+	always_ff @(posedge clk,posedge rst) begin
+		if(rst) begin
+			bird_y <= 16'd240;
+			bird_v <= 16'd0;
+		end else if(en) begin
+			bird_y <= nxt_bird_y;
+			bird_v <= nxt_bird_v;
+		end
 	end
+	
+	always_comb begin
+		if(jmp) nxt_bird_v = JUMP_VEL;
+		else    nxt_bird_v = bird_v - BIRD_ACC;
+		nxt_bird_y = bird_y + bird_v;
+	end
+	assign pos = bird_y;
 endmodule
 
 //the bird should be at the 160 pixels from the left border
