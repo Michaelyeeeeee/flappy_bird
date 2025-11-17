5489106479033a267db1c50e9fad1840d43d8c34
michael <michaeldmwu@gmail.com>
2025-11-17 20:15:50 +0000

    commit

--- source/vga_top.sv
+++ source/vga_top.sv
@@ -2,15 +2,20 @@ module vga_top (
 
 );
 
-logic c;
+logic [2:0] c;
+logic [9:0] x_pos, y_pos;
 
 vga u0(
-.game_clk,
+.game_clk(),
 .rgb(c),
 .hsync(),
 .vsync(),
-.x_pos(),
-.y_pos(),
+.x_pos(x_pos),
+.y_pos(y_pos),
 .color()
+);
+
+vga_test u1(
+
 );
 endmodule
