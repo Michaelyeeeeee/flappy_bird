a75c2c73219f9313b25d3d396206c9f83d55e1a8
coin50 <chu285@purdue.edu>
2025-11-30 23:37:34 -0500

    Create clockdivider.sv

---
+++ source/clockdivider.sv
@@ -0,0 +1,17 @@
+`define RATIO  10000
+module clockdivider(
+	input logic clk,rst,
+	output logic CLK
+);
+	int x,nx;
+	
+	always_ff @(posedge clk,posedge rst) begin
+		if(rst) x <= 1;
+		else x <= nx;
+	end
+	always_comb begin
+		if( x == `RATIO ) nx = 1;
+		else              nx = x + 1;
+	end
+	assign CLK = x == 1;
+endmodule
