763b2e454bdd3b5b5c898249202429f6cf835877
SHamilton2024 <124546340+SHamilton2024@users.noreply.github.com>
2025-11-30 23:01:28 -0500

    Update and rename game_top.sv to vga_game_top.sv
    Yellow bird on blue screen

--- source/vga_game_top.sv
+++ source/vga_game_top.sv
@@ -1,5 +1,6 @@
-module game_top(
+module vga_game_top(
     input logic clk,
+    input logic button,
     output logic [2:0] color
 );
     parameter X_SIZE = 10'd799;  // maximum x size
@@ -7,12 +8,16 @@ module game_top(
 
     logic [9:0] x = 10'd0;
     logic [9:0] y = 10'd0;
+    logic buttonpress = 1'b0;
+
+    //logic button_last,press_edge;
 
     always_ff @ (posedge clk) begin
-         if ((y < 50) && x < 50) begin
-            color = 3'b010;
+
+        if (((y > 210) && (y < 240)) && ((x < 170) && (x > 147)) )begin
+            color <= 3'b110;
         end else begin
-            color = 3'b011;
+            color <= 3'b011;
         end
         
         if (x == X_SIZE) begin
@@ -27,7 +32,15 @@ module game_top(
         end else begin
             x <= x + 10'd1;
         end
-    end
-    //assign color = 3'b011;
 
-endmodule
+        if (buttonpress) begin
+            color <= ~color;
+        end
+    end
+
+    always_ff @ (negedge button) begin
+        buttonpress <= ~buttonpress;
+    end
+
+
+endmodule 
